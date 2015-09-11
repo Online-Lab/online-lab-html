bcc95ee025a3d6b7f59d20a482db9603af8ed143
ds007007 <ds007007@yandex.ru>
2015-09-11 15:45:25 +0300

    postInstall.js fix

--- postInstall.js
+++ postInstall.js
@@ -5,7 +5,7 @@ var is_production = process.env.NODE_ENV == "production";
 if (is_production) {
   console.log('running bower install');
   exec('node node_modules/bower/bin/bower install');
-  console.log('running grunt grunt build:dist');
+  console.log('running grunt build:dist');
   exec('node node_modules/grunt-cli/bin/grunt grunt build:dist');
   return;
 }
