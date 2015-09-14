092e5b2d9a3fff5766d592e047aac88b930a8574
ds007007 <ds007007@yandex.ru>
2015-09-14 13:09:22 +0300

    Some missing resources fixed

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,13 +2,5 @@
 
 angular.module('onlinelabApp')
   .controller('MainCtrl', function ($scope, $http) {
-	
-	$scope.myvar = 14;
-    $scope.awesomeThings = [];
-	$scope.current = 1;
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
-
-  });
+});
--- client/app/project/project.controller.js
+++ client/app/project/project.controller.js
@@ -2,13 +2,5 @@
 
 angular.module('onlinelabApp')
   .controller('ProjectCtrl', function ($scope, $http, $state) {
-	
-	$scope.myvar = 14;
-    $scope.awesomeThings = [];
-	$scope.current = 1;
-console.log($state);
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
 
   });
