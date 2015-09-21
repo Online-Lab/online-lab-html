98325a30806354d1bb20db08f56e64efb520489b
ds007007 <ds007007@yandex.ru>
2015-09-21 15:05:18 +0300

    Real phones in header

--- client/app/header_directive/header_directive.js
+++ client/app/header_directive/header_directive.js
@@ -6,22 +6,20 @@ angular.module('onlinelabApp')
 	.directive('appHeader', ['$state', function ($state) {
         return {
             restrict: 'E',
-            link: function (scope) {
-      				scope.current_city = 3;
-      				scope.city_phone = '+7 960 222 22 22';
-      			
+            link: function (scope) {      			
       				scope.city_items = [
-      				{city:'Москва', 			number: '+7 960 000 00 00', active:'', id:1},
-      				{city:'Тамбов',				number: '+7 960 222 22 22', active:'', id:3},
-      				{city:'Нью-Йорк', 			number: '+7 960 333 33 33', active:'', id:4}
+        				{ city:'Москва',	phone: '+7 (499) 348-24-43', active:'', id:1 },
+        				{ city:'Тамбов',	phone: '+7 (4752) 341-341', active:'', id:3 },
+        				{ city:'Нью-Йорк',	phone: '+1 (347) 681 244', active:'', id:4 }
       				];
-      				scope.changeCity = function (){
-      					
-      				};
-      				scope.city_click = function (item){
-      					scope.city_phone = item.number;
-      					scope.current_city = item.id;
-      				};
+
+              scope.city_phone = scope.city_items[1].phone;
+              scope.current_city = scope.city_items[1].id;
+
+      				scope.changeCity = function (city){
+      					scope.city_phone = city.phone;
+      					scope.current_city = city.id;
+              };
       				
       				scope.state = $state;
 
