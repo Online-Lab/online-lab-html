77ce168061551d809999884ed3237c10695056f7
ds007007 <ds007007@yandex.ru>
2015-09-21 15:23:20 +0300

    Blogs pager disabled

--- client/app/contact/contact.controller.js
+++ client/app/contact/contact.controller.js
@@ -4,10 +4,24 @@ angular.module('onlinelabApp')
   .controller('ContactCtrl', function ($scope) {
     $scope.marks = [
       {
+        address: "Тамбов, Кавалерийская 7а",
+        phone: "+7(4752) 341-341",
         pos: {x: 100, y: 100}
       },
       {
-        pos: {x: 300, y: 500}
+        address: "Москва, Ленинский пр-т 30",
+        phone: "+7(499) 348-24-43",
+        pos: {x: 100, y: 100}
+      },
+      {
+        address: "Rävala puiestee 7, 10145 Tallinn, Эстония",
+        phone: "+372 618 8006",
+        pos: {x: 100, y: 100}
+      },
+      {
+        address: "54 W 40th St New York, NY 10018",
+        phone: " +1(347) 681 244",
+        pos: {x: 100, y: 100}
       }
     ];
   });
