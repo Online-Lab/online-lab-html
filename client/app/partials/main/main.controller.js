c5f835c5324ca488afbe16ef452758d1e8b7956c
ds007007 <ds007007@yandex.ru>
2015-09-23 17:37:03 +0300

    Main page portfolio slider

--- client/app/partials/main/main.controller.js
+++ client/app/partials/main/main.controller.js
@@ -2,5 +2,26 @@
 
 angular.module('onlinelabApp')
   .controller('MainCtrl', function ($scope, $http) {
-    console.log("Main controller");
+
+    $scope.portfolioList = undefined;
+    $http.get('/data/db.json').success(function(data){
+      $scope.portfolioList = data.portfolio;
+      //Current item
+      $scope.currentPortfolioItem = $scope.portfolioList[0];
+    });
+
+    //$scope.portfolioSliderLeft = 0;
+    $scope.gotoProject = function(itemIndex, item) {
+      //Current item
+      $scope.currentPortfolioItem = item;
+
+      //Scroll
+      var itemWidth = 234;
+      var left = - itemWidth * itemIndex + (itemWidth + itemWidth);
+      //correct position
+      if (left > 0) 
+        left = 0;
+      //animate
+      $(".cd-slider-nav nav").animate({left: left}, 500);
+    };
 });
