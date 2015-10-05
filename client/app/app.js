1077afb15ccd3928a3e493829242ca975f941084
ds007007 <ds007007@yandex.ru>
2015-10-05 13:47:28 +0300

    Scroll to top when url changes

--- client/app/app.js
+++ client/app/app.js
@@ -105,4 +105,18 @@ angular.module('onlinelabApp', [
       .otherwise('/');
 
     $locationProvider.html5Mode(true);
+  })
+
+  .run(function($anchorScroll, $window) {
+    //Hack to scroll to top when navigating to new URLS but not back/forward
+    var wrap = function(method) {
+      var orig = $window.window.history[method];
+      $window.window.history[method] = function() {
+        var retval = orig.apply(this, Array.prototype.slice.call(arguments));
+        $anchorScroll();
+        return retval;
+      };
+    };
+    wrap('pushState');
+    wrap('replaceState');
   });
