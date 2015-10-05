aeefe8e9d62306dfd1c09bbc783bcd042d32156e
ds007007 <ds007007@yandex.ru>
2015-10-05 15:30:30 +0300

    Absolute images urls in team-directive

--- client/app/team-gallery_directive/team-gallery_directive.js
+++ client/app/team-gallery_directive/team-gallery_directive.js
@@ -8,68 +8,68 @@ angular.module('onlinelabApp')
       link: function (scope, element, attrs) {
         scope.teamPhotos = [
           {
-            smallPhoto: "../../assets/images/team_gallery/small-1.jpg",
-            largePhoto: "../../assets/images/team_gallery/large-1.jpg"
+            smallPhoto: "/assets/images/team_gallery/small-1.jpg",
+            largePhoto: "/assets/images/team_gallery/large-1.jpg"
           },
           {
-            smallPhoto: "../../assets/images/team_gallery/small-2.jpg",
-            largePhoto: "../../assets/images/team_gallery/large-2.jpg"
+            smallPhoto: "/assets/images/team_gallery/small-2.jpg",
+            largePhoto: "/assets/images/team_gallery/large-2.jpg"
           },
           {
-            smallPhoto: "../../assets/images/team_gallery/small-3.jpg",
-            largePhoto: "../../assets/images/team_gallery/large-3.jpg"
+            smallPhoto: "/assets/images/team_gallery/small-3.jpg",
+            largePhoto: "/assets/images/team_gallery/large-3.jpg"
           },
           {
-            smallPhoto: "../../assets/images/team_gallery/small-4.jpg",
-            largePhoto: "../../assets/images/team_gallery/large-4.jpg"
+            smallPhoto: "/assets/images/team_gallery/small-4.jpg",
+            largePhoto: "/assets/images/team_gallery/large-4.jpg"
           },
           {
-            smallPhoto: "../../assets/images/team_gallery/small-5.jpg",
-            largePhoto: "../../assets/images/team_gallery/large-5.jpg"
+            smallPhoto: "/assets/images/team_gallery/small-5.jpg",
+            largePhoto: "/assets/images/team_gallery/large-5.jpg"
           },
           {
-            smallPhoto: "../../assets/images/team_gallery/small-6.jpg",
-            largePhoto: "../../assets/images/team_gallery/large-6.jpg"
+            smallPhoto: "/assets/images/team_gallery/small-6.jpg",
+            largePhoto: "/assets/images/team_gallery/large-6.jpg"
           },
           {
-            smallPhoto: "../../assets/images/team_gallery/small-7.jpg",
-            largePhoto: "../../assets/images/team_gallery/large-7.jpg"
+            smallPhoto: "/assets/images/team_gallery/small-7.jpg",
+            largePhoto: "/assets/images/team_gallery/large-7.jpg"
           },
           {
-            smallPhoto: "../../assets/images/team_gallery/small-8.jpg",
-            largePhoto: "../../assets/images/team_gallery/large-8.jpg"
+            smallPhoto: "/assets/images/team_gallery/small-8.jpg",
+            largePhoto: "/assets/images/team_gallery/large-8.jpg"
           },
           {
-            smallPhoto: "../../assets/images/team_gallery/small-9.jpg",
-            largePhoto: "../../assets/images/team_gallery/large-9.jpg"
+            smallPhoto: "/assets/images/team_gallery/small-9.jpg",
+            largePhoto: "/assets/images/team_gallery/large-9.jpg"
           },
           {
-            smallPhoto: "../../assets/images/team_gallery/small-10.jpg",
-            largePhoto: "../../assets/images/team_gallery/large-10.jpg"
+            smallPhoto: "/assets/images/team_gallery/small-10.jpg",
+            largePhoto: "/assets/images/team_gallery/large-10.jpg"
           },
           {
-            smallPhoto: "../../assets/images/team_gallery/small-11.jpg",
-            largePhoto: "../../assets/images/team_gallery/large-11.jpg"
+            smallPhoto: "/assets/images/team_gallery/small-11.jpg",
+            largePhoto: "/assets/images/team_gallery/large-11.jpg"
           },
           {
-            smallPhoto: "../../assets/images/team_gallery/small-12.jpg",
-            largePhoto: "../../assets/images/team_gallery/large-12.jpg"
+            smallPhoto: "/assets/images/team_gallery/small-12.jpg",
+            largePhoto: "/assets/images/team_gallery/large-12.jpg"
           },
           {
-            smallPhoto: "../../assets/images/team_gallery/small-13.jpg",
-            largePhoto: "../../assets/images/team_gallery/large-13.jpg"
+            smallPhoto: "/assets/images/team_gallery/small-13.jpg",
+            largePhoto: "/assets/images/team_gallery/large-13.jpg"
           },
           {
-            smallPhoto: "../../assets/images/team_gallery/small-14.jpg",
-            largePhoto: "../../assets/images/team_gallery/large-14.jpg"
+            smallPhoto: "/assets/images/team_gallery/small-14.jpg",
+            largePhoto: "/assets/images/team_gallery/large-14.jpg"
           },
           {
-            smallPhoto: "../../assets/images/team_gallery/small-15.jpg",
-            largePhoto: "../../assets/images/team_gallery/large-15.jpg"
+            smallPhoto: "/assets/images/team_gallery/small-15.jpg",
+            largePhoto: "/assets/images/team_gallery/large-15.jpg"
           },
           {
-            smallPhoto: "../../assets/images/team_gallery/small-16.jpg",
-            largePhoto: "../../assets/images/team_gallery/large-16.jpg"
+            smallPhoto: "/assets/images/team_gallery/small-16.jpg",
+            largePhoto: "/assets/images/team_gallery/large-16.jpg"
           }
         ];
 
