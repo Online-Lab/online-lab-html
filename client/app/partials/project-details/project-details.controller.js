68c11093ddbd8f90e1d8a62e74dde867fe61b35d
ds007007 <ds007007@yandex.ru>
2015-10-07 12:09:09 +0300

    Header image for project-details page

--- client/app/partials/project-details/project-details.controller.js
+++ client/app/partials/project-details/project-details.controller.js
@@ -5,11 +5,12 @@ angular.module('onlinelabApp')
     //Current project id
     $scope.projectId = $state.params.projectId;
     
+    $state.project_image = '';
     //Get project
     $scope.project = Project.get({projectId: $scope.projectId}, function(){
       //Save project header_image to state
       $state.project_image = $scope.project.header_image;
+      
+      console.log($state.project_image);
     });
-    
-    console.dir($scope.project);
   });
