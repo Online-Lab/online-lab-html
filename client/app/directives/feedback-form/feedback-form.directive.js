d0ed07e0d7ff168662d3c84c734c75bded19d36c
ds007007 <ds007007@yandex.ru>
2015-10-07 15:49:46 +0300

    feedback-form controller code replaced into directive

--- client/app/directives/feedback-form/feedback-form.directive.js
+++ client/app/directives/feedback-form/feedback-form.directive.js
@@ -5,8 +5,64 @@ angular.module('onlinelabApp')
     return {
       templateUrl: 'app/directives/feedback-form/feedback-form.html',
       restrict: 'EA',
-      link: function (scope, element, attrs) {
+      scope: {
         
+      },
+      link: function ($scope, elem, attrs) {
+        //User data
+        $scope.newOrderData = {
+          name: "",
+          phone: "",
+          email: ""
+        };
+        
+        //Feedback form message
+        $scope.formMessage = "";
+        $scope.isFormValid = true;
+    
+        //Get consultation
+        $scope.order = function(){
+
+          if ($scope.newOrderData.name == ''){
+            $scope.isFormValid = false;
+            $scope.formMessage = "Введите имя";
+            console.log($scope.isFormValid);
+          }
+          else if ($scope.newOrderData.phone == ''){
+            $scope.isFormValid = false;
+            $scope.formMessage = "Введите номер телефона";
+          }
+          else if ($scope.newOrderData.email == ''){
+            $scope.isFormValid = false;
+            $scope.formMessage = "Введите корректный адрес электронной почты";
+          }
+          else{
+            $scope.isFormValid = true;
+            $scope.formMessage = "Подождите...";
+            
+            //Sending email
+            var msg = "Была добавлена новая заявка. Информация о клиенте: <br><br>" + 
+              ($scope.newOrderData.name ? "Имя: " + $scope.newOrderData.name + "<br>" : '') + 
+              ($scope.newOrderData.email ? "E-mail: " + $scope.newOrderData.email + "<br>" : '') + 
+              ($scope.newOrderData.phone ? "Тел.: " + $scope.newOrderData.phone + "<br>" : '');
+              
+            alert("Заявка отправлена");
+            clearForm();
+
+            /*Mailer.sendEmail(msg).success(function(){
+              //alert("Ваша заявка принята");
+              $scope.formMessage = "Ваша заявка принята";
+              //Clear fields
+              clearForm();
+              
+            });*/
+          }
+          
+        };
+        
+        var clearForm = function(){
+          
+        };
       }
     };
   });
