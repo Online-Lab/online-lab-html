27c7253ecb7a02d43a5c4c4ff6b9030616f0430e
ds007007 <ds007007@yandex.ru>
2015-10-14 17:29:14 +0300

    Last store projects, projects filter

--- client/app/partials/store/store.controller.js
+++ client/app/partials/store/store.controller.js
@@ -2,16 +2,18 @@
 
 angular.module('onlinelabApp')
   .controller('StoreCtrl', function ($scope, Project) {
+    
     //Count of projects to display
     $scope.lastProjectsDisplayCount = 3;
     
-    //Get last projects
-    $scope.lastProjectsList = Project.query(function(){
+    //Get last projects of type
+    var lastProjectsFilter = 'store';
+    $scope.lastProjectsList = Project.query({kind: lastProjectsFilter}, function(){
       //Correct count of projects to display
       if ($scope.lastProjectsList.length < 3)
         $scope.lastProjectsDisplayCount = $scope.lastProjectsList.length;
     });
-    
+        
     //Show more button logic
     $scope.showMoreProjects = function(){
       if ($scope.lastProjectsList.length <= 3) 
