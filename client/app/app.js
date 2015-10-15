70c2547281861d01f0abc957edc53cdb2a9a5b7c
ds007007 <ds007007@yandex.ru>
2015-10-15 15:59:35 +0300

    Title directive, SEO titles

--- client/app/app.js
+++ client/app/app.js
@@ -17,95 +17,95 @@ angular.module('onlinelabApp', [
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
     $stateProvider
       .state('main', {
-      url: '/',
-      templateUrl: 'app/partials/main/main.html',
-      controller: 'MainCtrl',
-      ncyBreadcrumb: {
-        label: 'Главная'
-      }
+        url: '/',
+        templateUrl: 'app/partials/main/main.html',
+        controller: 'MainCtrl',
+        ncyBreadcrumb: { label: 'Главная' },
+        title: 'Сетевая лаборатория – digital-агентство сетевых разработок и маркетинговых решений'
       })
-    .state('development', {
-      url: '/development',
-      templateUrl: 'app/partials/development/development.html',
-      controller: 'DevelopmentCtrl',
-      ncyBreadcrumb: {
-        label: 'Web разработка'
-      }
-      })    
-    .state('projects', {
-      url: '/projects',
-      templateUrl: 'app/partials/projects/projects.html',
-      controller: 'ProjectsCtrl',
-      ncyBreadcrumb: {
-        label: 'Портфолио'
-      }
+      .state('development', {
+        url: '/development',
+        templateUrl: 'app/partials/development/development.html',
+        controller: 'DevelopmentCtrl',
+        ncyBreadcrumb: { label: 'Web разработка' },
+        title: 'Создание сайтов | Разработка интернет-проектов – Online-lab'
       })
-    .state('project-details', {
-      url: '/projects/:projectId',
-      templateUrl: 'app/partials/project-details/project-details.html',
-      controller: 'ProjectDetailsCtrl',
-      ncyBreadcrumb: {
-        label: '{{projectName}}',
-        parent: 'projects'
-      }
-      })
-    .state('blog', {
-      url: '/blog',
-      templateUrl: 'app/partials/blog/blog.html',
-      controller: 'BlogCtrl',
-      ncyBreadcrumb: {
-        label: 'Наш блог'
-      }
-      })
-    .state('blogpost', {
-      url: '/blogpost',
-      templateUrl: 'app/partials/blogpost/blogpost.html',
-      controller: 'BlogPostCtrl',
-      ncyBreadcrumb: {
-        label: 'Запись блога'
-      }
-      })
-    .state('team', {
-      url: '/team',
-      templateUrl: 'app/partials/team/team.html',
-      controller: 'TeamCtrl',
-      ncyBreadcrumb: { label: 'О нас' }
-      })
-    .state('mobile', {
+      .state('mobile', {
         url: '/mobile',
         templateUrl: 'app/partials/mobile/mobile.html',
         controller: 'MobileCtrl',
-        ncyBreadcrumb: { label: 'Мобильные приложения' }
+        ncyBreadcrumb: { label: 'Мобильные приложения' },
+        title: 'Создание мобильных приложений | разработка мобильных приложений для IOS и Android'
       })
-    .state('electronics', {
+      .state('electronics', {
         url: '/electronics',
         templateUrl: 'app/partials/electronics/electronics.html',
         controller: 'ElectronicsCtrl',
-        ncyBreadcrumb: { label: 'Электроника' }
+        ncyBreadcrumb: { label: 'Электроника' },
+        title: 'Разработка электронных устройств | Создание электронных устройств на заказ'
       })
-    .state('marketing', {
+      .state('marketing', {
         url: '/marketing',
         templateUrl: 'app/partials/marketing/marketing.html',
         controller: 'MarketingCtrl',
-        ncyBreadcrumb: { label: 'Интернет маркетинг' }
+        ncyBreadcrumb: { label: 'Интернет маркетинг' },
+        title: 'Комплексный Интернет-маркетинг | Агентство  интернет-продвижения и маркетинга Online-lab'
       })
-    .state('contact', {
-        url: '/contact',
-        templateUrl: 'app/partials/contact/contact.html',
-        controller: 'ContactCtrl',
-        ncyBreadcrumb: { label: 'Контакты' }
-      })
-    .state('vacancies', {
-        url: '/vacancies',
-        templateUrl: 'app/partials/vacancies/vacancies.html',
-        controller: 'VacanciesCtrl',
-        ncyBreadcrumb: { label: 'Вакансии' }
-      })
-    .state('store', {
+      .state('store', {
         url: '/store',
         templateUrl: 'app/partials/store/store.html',
         controller: 'StoreCtrl',
-        ncyBreadcrumb: { label: 'Интернет магазины' }
+        ncyBreadcrumb: { label: 'Интернет магазины' },
+        title: 'Разработка интернет-магазина | Создание сайта интернет-магазина под ключ'
+      })
+      .state('projects', {
+        url: '/projects',
+        templateUrl: 'app/partials/projects/projects.html',
+        controller: 'ProjectsCtrl',
+        ncyBreadcrumb: { label: 'Портфолио' },
+        title: 'Портфолио – Сетевая Лаборатория'
+      })
+      .state('project-details', {
+        url: '/projects/:projectId',
+        templateUrl: 'app/partials/project-details/project-details.html',
+        controller: 'ProjectDetailsCtrl',
+        ncyBreadcrumb: {
+          label: '{{projectName}}',
+          parent: 'projects'
+        }
+      })
+      .state('blog', {
+        url: '/blog',
+        templateUrl: 'app/partials/blog/blog.html',
+        controller: 'BlogCtrl',
+        ncyBreadcrumb: { label: 'Наш блог' }
+      })
+      .state('blogpost', {
+        url: '/blogpost',
+        templateUrl: 'app/partials/blogpost/blogpost.html',
+        controller: 'BlogPostCtrl',
+        ncyBreadcrumb: { label: 'Запись блога' }
+      })
+      .state('team', {
+        url: '/team',
+        templateUrl: 'app/partials/team/team.html',
+        controller: 'TeamCtrl',
+        ncyBreadcrumb: { label: 'О нас' },
+        title: 'О нас – Сетевая Лаборатория'
+      })
+      .state('contact', {
+        url: '/contact',
+        templateUrl: 'app/partials/contact/contact.html',
+        controller: 'ContactCtrl',
+        ncyBreadcrumb: { label: 'Контакты' },
+        title: 'Контакты – Сетевая Лаборатория'
+      })
+      .state('vacancies', {
+        url: '/vacancies',
+        templateUrl: 'app/partials/vacancies/vacancies.html',
+        controller: 'VacanciesCtrl',
+        ncyBreadcrumb: { label: 'Вакансии' },
+        title: 'Вакансии – Сетевая Лаборатория'
       });
   
   
---
+++ client/app/directives/title/title.js
@@ -0,0 +1,19 @@
+angular.module('onlinelabApp').directive('title', ['$rootScope', '$timeout',
+  function($rootScope, $timeout) {
+    return {
+      link: function() {
+
+        var listener = function(event, toState) {
+
+          $timeout(function() {
+            $rootScope.title = (toState.title) 
+            ? toState.title
+            : 'Сетевая лаборатория';
+          });
+        };
+
+        $rootScope.$on('$stateChangeSuccess', listener);
+      }
+    };
+  }
+]);
