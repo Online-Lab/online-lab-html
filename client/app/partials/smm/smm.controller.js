4ef56bad4c3200599fac13aefc81968f38e54828
ds007007 <ds007007@yandex.ru>
2015-10-21 13:13:05 +0300

    Last projects block on SMM page

--- client/app/partials/smm/smm.controller.js
+++ client/app/partials/smm/smm.controller.js
@@ -1,6 +1,33 @@
 'use strict';
 
 angular.module('onlinelabApp')
-  .controller('SmmCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('SmmCtrl', function ($scope Project) {
+    
+    //Count of projects to display
+    $scope.lastProjectsDisplayCount = 3;
+    
+    //Get last projects of type
+    var lastProjectsFilter = 'marketing';
+    $scope.lastProjectsList = Project.query({kind: lastProjectsFilter}, function(){
+      //Correct count of projects to display
+      if ($scope.lastProjectsList.length < 3)
+        $scope.lastProjectsDisplayCount = $scope.lastProjectsList.length;
+    });
+        
+    //Show more button logic
+    $scope.showMoreProjects = function(){
+      if ($scope.lastProjectsList.length <= 3) 
+        return;
+      
+      if ($scope.lastProjectsDisplayCount >= $scope.lastProjectsList.length)
+      {
+        $scope.lastProjectsDisplayCount = 3;
+        if ($scope.lastProjectsList.length < 3)
+          $scope.lastProjectsDisplayCount = $scope.lastProjectsList.length;
+      }
+      else{
+        $scope.lastProjectsDisplayCount += 3;
+      }
+    };
+    
   });
