79af74bb113ac0ae4e38983c4a69afbe00636f2a
ds007007 <ds007007@yandex.ru>
2015-10-23 14:49:58 +0300

    Last-projects directive for resource page

--- client/app/partials/resources/resources.controller.js
+++ client/app/partials/resources/resources.controller.js
@@ -1,38 +1,6 @@
 'use strict';
 
 angular.module('onlinelabApp')
-  .controller('ResourcesCtrl', function ($scope, Project) {
-        
-    //Count of projects to display
-    $scope.lastProjectsDisplayCount = 3;
-    
-    //Get last projects of type
-    var lastProjectsFilter = 'resource';
-    Project.filterByKind({kind: lastProjectsFilter}, function(responce){
-      $scope.lastProjectsList = responce.data;
-      
-      //Correct count of projects to display
-      if ($scope.lastProjectsList.length < 3)
-        $scope.lastProjectsDisplayCount = $scope.lastProjectsList.length;
-    }, 
-    function(error){
-      
-    });
-        
-    //Show more button logic
-    $scope.showMoreProjects = function(){
-      if ($scope.lastProjectsList.length <= 3) 
-        return;
-      
-      if ($scope.lastProjectsDisplayCount >= $scope.lastProjectsList.length)
-      {
-        $scope.lastProjectsDisplayCount = 3;
-        if ($scope.lastProjectsList.length < 3)
-          $scope.lastProjectsDisplayCount = $scope.lastProjectsList.length;
-      }
-      else{
-        $scope.lastProjectsDisplayCount += 3;
-      }
-    };
+  .controller('ResourcesCtrl', function ($scope) {
     
   });
