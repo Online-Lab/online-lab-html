c052274e1f72b672d876ecd76d6ebccb85c098af
ds007007 <ds007007@yandex.ru>
2015-10-23 14:51:36 +0300

    Last-projects directive for smm page

--- client/app/partials/smm/smm.controller.js
+++ client/app/partials/smm/smm.controller.js
@@ -1,38 +1,5 @@
 'use strict';
 
 angular.module('onlinelabApp')
-  .controller('SmmCtrl', function ($scope, Project) {
-    
-    //Count of projects to display
-    $scope.lastProjectsDisplayCount = 3;
-    
-    //Get last projects of type
-    var lastProjectsFilter = 'marketing';
-    Project.filterByKind({kind: lastProjectsFilter}, function(responce){
-      $scope.lastProjectsList = responce.data;
-      
-      //Correct count of projects to display
-      if ($scope.lastProjectsList.length < 3)
-        $scope.lastProjectsDisplayCount = $scope.lastProjectsList.length;
-    },
-    function(error){
-      
-    });
-        
-    //Show more button logic
-    $scope.showMoreProjects = function(){
-      if ($scope.lastProjectsList.length <= 3) 
-        return;
-      
-      if ($scope.lastProjectsDisplayCount >= $scope.lastProjectsList.length)
-      {
-        $scope.lastProjectsDisplayCount = 3;
-        if ($scope.lastProjectsList.length < 3)
-          $scope.lastProjectsDisplayCount = $scope.lastProjectsList.length;
-      }
-      else{
-        $scope.lastProjectsDisplayCount += 3;
-      }
-    };
-    
+  .controller('SmmCtrl', function ($scope) {
   });
