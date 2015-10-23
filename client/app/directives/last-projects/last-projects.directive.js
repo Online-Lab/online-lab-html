6380694a2e852a1513c6907648eb472aec452088
ds007007 <ds007007@yandex.ru>
2015-10-23 14:35:35 +0300

    Last-projects directive: controller code

--- client/app/directives/last-projects/last-projects.directive.js
+++ client/app/directives/last-projects/last-projects.directive.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('onlinelabApp')
-  .directive('appLastProjects', function () {
+  .directive('appLastProjects', function (Project) {
     return {
       templateUrl: 'app/directives/last-projects/last-projects.html',
       restrict: 'A',
@@ -10,7 +10,35 @@ angular.module('onlinelabApp')
         filterBy: "@"
       },
       link: function (scope, element, attrs) {
-        
+
+        //Get last projects of type
+        var lastProjectsFilter = scope.filterBy;
+        Project.filterByKind({kind: lastProjectsFilter}, function(responce){
+          scope.lastProjectsList = responce.data;
+          //Correct count of projects to display
+          if (scope.lastProjectsList.length < 3)
+            scope.lastProjectsDisplayCount = scope.lastProjectsList.length;
+        },
+        function(error){
+          
+        });
+            
+        //Show more button logic
+        scope.showMoreProjects = function(){
+          if (scope.lastProjectsList.length <= 3) 
+            return;
+          
+          if (scope.lastProjectsDisplayCount >= scope.lastProjectsList.length)
+          {
+            scope.lastProjectsDisplayCount = 3;
+            if (scope.lastProjectsList.length < 3)
+              scope.lastProjectsDisplayCount = scope.lastProjectsList.length;
+          }
+          else{
+            scope.lastProjectsDisplayCount += 3;
+          }
+        };
+
       }
     };
   });
