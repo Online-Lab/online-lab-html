f88c9e67f53ae29ead50d897dbdfe35750b7e569
ds007007 <ds007007@yandex.ru>
2015-10-23 14:38:58 +0300

    Last-projects directive for mobile page

--- client/app/partials/mobile/mobile.controller.js
+++ client/app/partials/mobile/mobile.controller.js
@@ -1,37 +1,6 @@
 'use strict';
 
 angular.module('onlinelabApp')
-  .controller('MobileCtrl', function ($scope, Project) {
-    
-    //Count of projects to display
-    $scope.lastProjectsDisplayCount = 3;
-    
-    //Get last projects of type
-    var lastProjectsFilter = 'mobile_app';
-    Project.filterByKind({kind: lastProjectsFilter}, function(responce){
-      $scope.lastProjectsList = responce.data;
-      //Correct count of projects to display
-      if ($scope.lastProjectsList.length < 3)
-        $scope.lastProjectsDisplayCount = $scope.lastProjectsList.length;
-    },
-    function(error){
-      
-    });
-        
-    //Show more button logic
-    $scope.showMoreProjects = function(){
-      if ($scope.lastProjectsList.length <= 3) 
-        return;
-      
-      if ($scope.lastProjectsDisplayCount >= $scope.lastProjectsList.length)
-      {
-        $scope.lastProjectsDisplayCount = 3;
-        if ($scope.lastProjectsList.length < 3)
-          $scope.lastProjectsDisplayCount = $scope.lastProjectsList.length;
-      }
-      else{
-        $scope.lastProjectsDisplayCount += 3;
-      }
-    };
+  .controller('MobileCtrl', function ($scope) {
     
   });
