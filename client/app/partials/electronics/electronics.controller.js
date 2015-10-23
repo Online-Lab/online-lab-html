7ccf64a8de0c40bc0b9103cc129aa669e589297d
ds007007 <ds007007@yandex.ru>
2015-10-23 14:46:36 +0300

    Last-projects directive for electronics page

--- client/app/partials/electronics/electronics.controller.js
+++ client/app/partials/electronics/electronics.controller.js
@@ -1,38 +1,6 @@
 'use strict';
 
 angular.module('onlinelabApp')
-  .controller('ElectronicsCtrl', function ($scope, Project) {
+  .controller('ElectronicsCtrl', function ($scope) {
 
-    //Count of projects to display
-    $scope.lastProjectsDisplayCount = 3;
-    
-    //Get last projects of type
-    var lastProjectsFilter = 'electronics';
-    Project.filterByKind({kind: lastProjectsFilter}, function(responce){
-      $scope.lastProjectsList = responce.data;
-      
-      //Correct count of projects to display
-      if ($scope.lastProjectsList.length < 3)
-        $scope.lastProjectsDisplayCount = $scope.lastProjectsList.length;
-    },
-    function(error){
-      
-    });
-        
-    //Show more button logic
-    $scope.showMoreProjects = function(){
-      if ($scope.lastProjectsList.length <= 3) 
-        return;
-      
-      if ($scope.lastProjectsDisplayCount >= $scope.lastProjectsList.length)
-      {
-        $scope.lastProjectsDisplayCount = 3;
-        if ($scope.lastProjectsList.length < 3)
-          $scope.lastProjectsDisplayCount = $scope.lastProjectsList.length;
-      }
-      else{
-        $scope.lastProjectsDisplayCount += 3;
-      }
-    };
-    
   });
