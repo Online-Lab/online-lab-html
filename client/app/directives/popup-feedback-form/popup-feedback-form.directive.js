75bcf7c961e94beb5f955cacf1fe7e2f2097dfc2
ds007007 <ds007007@yandex.ru>
2015-10-23 15:45:49 +0300

    Popup-feedback-form: controller code, error classes, message box

--- client/app/directives/popup-feedback-form/popup-feedback-form.directive.js
+++ client/app/directives/popup-feedback-form/popup-feedback-form.directive.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('onlinelabApp')
-  .directive('appPopupFeedbackForm', function () {
+  .directive('appPopupFeedbackForm', function (Mailer) {
     return {
       templateUrl: 'app/directives/popup-feedback-form/popup-feedback-form.html',
       restrict: 'EA',
@@ -13,7 +13,8 @@ angular.module('onlinelabApp')
         var nameField = $(elem).find('.popup-feedback-form-name'),
             phoneField = $(elem).find('.popup-feedback-form-phone'),
             emailField = $(elem).find('.popup-feedback-form-email'),
-            orderBtn = $(elem).find('.popup-feedback-form-btn');
+            orderBtn = $(elem).find('.popup-feedback-form-btn'),
+            messageBlock = $(elem).find('.popup-feedback-form-msg');
             
         phoneField.inputmask("+7(999)999-99-99", {
           "clearIncomplete": true
@@ -21,7 +22,63 @@ angular.module('onlinelabApp')
         
         //Get consultation
         $scope.order = function(){
-          alert("Click");
+
+          var patternEmail = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+          //reset error classes
+          nameField.removeClass("error");
+          phoneField.removeClass("error");
+          emailField.removeClass("error");
+          
+          if (nameField.val() == ''){
+            messageBlock.text("Введите имя");
+            nameField.addClass("error");
+            nameField.focus();
+          }
+          else if (phoneField.val() == ''){
+            messageBlock.text("Введите номер телефона");
+            phoneField.addClass('error');
+            phoneField.focus();
+          }
+          else if (emailField.val() == '' ||  !patternEmail.test(emailField.val())){
+            messageBlock.text("Введите корректный e-mail адрес");
+            emailField.addClass("error");
+            emailField.focus();
+          }
+          else{
+            messageBlock.text("Подождите...");
+            orderBtn.prop('disabled', true);
+            
+            //User data
+            var newOrderData = {
+              name: nameField.val(),
+              phone: phoneField.val(),
+              email: emailField.val()
+            };
+            //Sending email
+            var msg = "Была добавлена новая заявка. Информация о клиенте: <br><br>" + 
+              (newOrderData.name ? "Имя: " + newOrderData.name + "<br>" : '') + 
+              (newOrderData.email ? "E-mail: " + newOrderData.email + "<br>" : '') + 
+              (newOrderData.phone ? "Тел.: " + newOrderData.phone + "<br>" : '');
+              
+            /*setTimeout(function(){
+              messageBlock.text("Ваша заявка принята!");
+              clearForm();
+            }, 3000);*/
+            
+            Mailer.sendEmail(msg).success(function(){
+              messageBlock.text("Ваша заявка принята");
+              clearForm();
+            });
+          }
+          
+        };
+        
+        function clearForm() {
+          nameField.val('');
+          phoneField.val('');
+          emailField.val('');
+          orderBtn.prop('disabled', false);
+          nameField.focus();
         };
       }
     };
