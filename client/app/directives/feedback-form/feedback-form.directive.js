4b14b7b9f9d643ce91ce07bf58bd3a5c72f0357b
ds007007 <ds007007@yandex.ru>
2015-10-29 13:53:26 +0300

    Feedback form code fix

--- client/app/directives/feedback-form/feedback-form.directive.js
+++ client/app/directives/feedback-form/feedback-form.directive.js
@@ -8,7 +8,7 @@ angular.module('onlinelabApp')
       scope: {
         
       },
-      link: function ($scope, elem, attrs) {
+      link: function (scope, elem, attrs) {
         //Form elements
         var nameField = $(elem).find('.main-feedback-form-name'),
             phoneField = $(elem).find('.main-feedback-form-phone'),
@@ -17,16 +17,16 @@ angular.module('onlinelabApp')
             messageBlock = $(elem).find('.main-feedback-form-msg');
             
         //User data
-        $scope.newOrderData = {
+        scope.newOrderData = {
           name: "",
           phone: "",
           email: ""
         };
         //Phone mask
-        $scope.phoneMask = "+7(999)999-99-99";
+        scope.phoneMask = "+7(999)999-99-99";
     
         //Get consultation
-        $scope.order = function(){
+        scope.order = function(){
 
           var patternEmail = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
           //reset error classes
@@ -34,17 +34,17 @@ angular.module('onlinelabApp')
           phoneField.removeClass("error");
           emailField.removeClass("error");
           
-          if ($scope.newOrderData.name == ''){
+          if (scope.newOrderData.name == ''){
             messageBlock.text("Введите имя");
             nameField.addClass("error");
             nameField.focus();
           }
-          else if ($scope.newOrderData.phone == ''){
+          else if (scope.newOrderData.phone == ''){
             messageBlock.text("Введите номер телефона");
             phoneField.addClass('error');
             phoneField.focus();
           }
-          else if ($scope.newOrderData.email == '' ||  !patternEmail.test($scope.newOrderData.email)){
+          else if (scope.newOrderData.email == '' ||  !patternEmail.test(scope.newOrderData.email)){
             messageBlock.text("Введите корректный адрес электронной почты");
             emailField.addClass("error");
             emailField.focus();
@@ -55,29 +55,29 @@ angular.module('onlinelabApp')
             
             //Sending email
             var msg = "Была добавлена новая заявка. Информация о клиенте: <br><br>" + 
-              ($scope.newOrderData.name ? "Имя: " + $scope.newOrderData.name + "<br>" : '') + 
-              ($scope.newOrderData.email ? "E-mail: " + $scope.newOrderData.email + "<br>" : '') + 
-              ($scope.newOrderData.phone ? "Тел.: " + $scope.newOrderData.phone + "<br>" : '');
+              (scope.newOrderData.name ? "Имя: " + scope.newOrderData.name + "<br>" : '') + 
+              (scope.newOrderData.email ? "E-mail: " + scope.newOrderData.email + "<br>" : '') + 
+              (scope.newOrderData.phone ? "Тел.: " + scope.newOrderData.phone + "<br>" : '');
               
             setTimeout(function(){
               messageBlock.text("Ваша заявка принята");
               alert(msg);
-              clearForm();
+              resetForm();
             }, 3000);
 
             /*Mailer.sendEmail(msg).success(function(){
               messageBlock.text("Ваша заявка принята");
-              clearForm();
+              resetForm();
             });*/
           }
           
         };
         
-        var clearForm = function(){
-          $scope.$apply(function(){
-            $scope.newOrderData.name = "";
-            $scope.newOrderData.phone = "";
-            $scope.newOrderData.email = "";
+        var resetForm = function(){
+          scope.$apply(function(){
+            scope.newOrderData.name = "";
+            scope.newOrderData.phone = "";
+            scope.newOrderData.email = "";
           });
           orderBtn.prop('disabled', false);
           nameField.focus();
