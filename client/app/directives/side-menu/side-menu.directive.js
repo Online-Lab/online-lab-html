aab4b82abd0b4cb5c4dd8bfb822e0650c778372d
ds007007 <ds007007@yandex.ru>
2015-10-30 12:47:27 +0300

    Side-menu hide logic when page is refreshed or location was changed

--- client/app/directives/side-menu/side-menu.directive.js
+++ client/app/directives/side-menu/side-menu.directive.js
@@ -1,11 +1,20 @@
 'use strict';
 
 angular.module('onlinelabApp')
-  .directive('appSideMenu', function () {
+  .directive('appSideMenu', function ($rootScope) {
     return {
       templateUrl: 'app/directives/side-menu/side-menu.html',
       restrict: 'EA',
       link: function (scope, element, attrs) {
+        
+        // Listen to "$viewContentLoaded" event on $rootScope in order to hide side-menu when page is refreshed or location was changed
+        $rootScope.$on("$viewContentLoaded", function(event){
+          
+          if (isSideMenuActive)
+            showHideSideMenu();//tis function is defined in script on side-menu.html page
+          
+        });
+        
       }
     };
   });
